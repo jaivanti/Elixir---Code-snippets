81298ba215ecb0bab516826257fbda25bee7914a
Jaivanti Dhokey <62715739+jaivanti@users.noreply.github.com>
2022-10-05 11:04:19 +0530

    Create Hello_World.exs

---
+++ Hello_World.exs
@@ -0,0 +1,18 @@
+#For Hello world 
+
+defmodule Hello do
+  def world do
+    IO.puts("Hello, world")
+  end
+end
+
+Hello.world
+
+
+#output: Hello, world
+
+#Also, can put simply: IO.puts("hello world")
+
+
+
+
